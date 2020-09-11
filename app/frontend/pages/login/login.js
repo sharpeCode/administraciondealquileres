8815d7edbff4932d56865c4402646dbbd29db066
Fatima Gaona <ale.garcia1001@hotmail.com>
2020-09-10 23:29:33 -0300

    Subo cambios copiados de la carpeta de xampp 2

--- app/frontend/pages/login/login.js
+++ app/frontend/pages/login/login.js
@@ -1,37 +1,37 @@
 function validar() {
-    var datosLogin = mapToJson($('#formLogin').serializeArray());
-    console.log("recibiendo datos de logeo: ", datosLogin);
-
-    let uri = EndpointsEnum.LOGIN;
-    console.log("Volver al listado de inmuebles = " + uri);
+    // var datosLogin = mapToJson($('#formLogin').serializeArray());
+    // console.log("recibiendo datos de logeo: ", datosLogin);
+    //
+    // let uri = EndpointsEnum.LOGIN;
+    // console.log("Volver al listado de inmuebles = " + uri);
 
     let href = EndpointsEnum.VOLVER_MENU_PRINCIPAL;
 
-    var funcionAjax = $.ajax({
-        url: uri,
-        method: "POST",
-        data: {
-            action: "validar",
-            datosLogin: datosLogin
-        }
-    });
-    funcionAjax.done(function (retorno) {
-        console.debug("Done: ", retorno);
-        if (retorno == "ERROR1"){
-            window.alert("los campos de dni y contraseña deben ser completados");
-        }else if (retorno == "ERROR2") {
-            window.alert("el dni o contraseña son incorrectos");
-        }else{
-            location.href = href;
-        }
-    });
-    funcionAjax.fail(function (retorno) {
-        console.log("error al guardar user")
-    });
-    funcionAjax.always(function (retorno) {
-        console.log("volvi de guardar el user")
-    });
-    console.log("Fin llamada controller usuario");
+    // var funcionAjax = $.ajax({
+    //     url: uri,
+    //     method: "POST",
+    //     data: {
+    //         action: "validar",
+    //         datosLogin: datosLogin
+    //     }
+    // });
+    // funcionAjax.done(function (retorno) {
+    //     console.debug("Done: ", retorno);
+    //     if (retorno == "ERROR1"){
+    //         window.alert("los campos de dni y contraseña deben ser completados");
+    //     }else if (retorno == "ERROR2") {
+    //         window.alert("el dni o contraseña son incorrectos");
+    //     }else{
+    //         location.href = href;
+    //     }
+    // });
+    // funcionAjax.fail(function (retorno) {
+    //     console.log("error al guardar user")
+    // });
+    // funcionAjax.always(function (retorno) {
+    //     console.log("volvi de guardar el user")
+    // });
+    // console.log("Fin llamada controller usuario");
 }
 
 function ingresarAlSistema() {
